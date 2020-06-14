694e4125491f004dda07c06a4a49647c3910c955
maaush <mauribenitez95@gmail.com>
2020-06-14 16:43:35 -0300

    Finish exercise 5.1 and 5.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
-<<<<<<< HEAD
-import Login from './components/login/Loginform';
+import React, {useEffect, useState} from 'react';
+import Blog from './components/Blog';
 import Title from './components/Common/Title';
+import Login from './components/login/Loginform';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
+
+  useEffect( () => {
+    const data = blogService.getAll().then(blogs => setBlogs(blogs));
+  }, [])
+
+  useEffect(() => {
+
+    const loggedUserJSON = window.localStorage.getItem('LoggedBlogUser');
+
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.handleToken(user.token)
+    }
+  }, []);
+
+
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -14,48 +31,61 @@ const App = () => {
   const handleUsernameChange = (change) => setUsername(change)
   const handlePasswordChange = (change) => setPassword(change);
 
-  const handleLogin = () => {
+  const handleLogin = async (event) => {
 
+    event.preventDefault();
+
+    try {
+
+      const user = await loginService.login({
+        username, password
+      });
+
+      window.localStorage.setItem('LoggedBlogUser',
+        JSON.stringify(user))
+
+      setUsername('')
+      setPassword('')
+      setUser(user);
+    } catch (e) {
+      console.error(e.message)
+    }
+  }
+
+  const displayLoginForm = () => {
+    return (
+      <>
+        <Title text="Log in"/>
+        <br/>
+        <Login handleLogin={handleLogin} password={password} setPassword={handlePasswordChange}
+               username={username} setUsername={handleUsernameChange} />
+      </>
+    )
   }
 
   const displayBlogs = (blogs) => {
     return (
-      blogs.map(blog =>
-        <Blog blog={blog} id={blog.id}/>
-      )
+      <>
+        <Title text="Blogs"/>
+        <p>{user.name} logged in.</p>
+        {
+          blogs.map(blog =>
+            <Blog blog={blog} id={blog.id}/>
+          )}
+      </>
     );
   };
-=======
-
-const App = () => {
-  const [blogs, setBlogs] = useState([])
->>>>>>> 5bec4155a5768fe5a385e486cec11b143139c2bb
-
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-<<<<<<< HEAD
-    )
-=======
-    )  
->>>>>>> 5bec4155a5768fe5a385e486cec11b143139c2bb
-  }, [])
 
   return (
     <div>
-<<<<<<< HEAD
-      <Login handleLogin={handleLogin} password={password} setPassword={handlePasswordChange}
-             username={username} setUsername={handleUsernameChange} />
-      <Title text='blogs'/>
-      {displayBlogs(blogs)}
-=======
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
->>>>>>> 5bec4155a5768fe5a385e486cec11b143139c2bb
+
+      {user === null ?
+
+        displayLoginForm() :
+        displayBlogs(blogs)
+      }
     </div>
   )
 }
 
-export default App
+export default App;
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-<<<<<<< HEAD
-let token;
+let token = null;
 
-const handleToken = (newToken) => {
+const setToken = (newToken) => {
   token = `bearer ${newToken}`;
 };
 
@@ -12,11 +11,6 @@ const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data;
 };
-=======
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
->>>>>>> 5bec4155a5768fe5a385e486cec11b143139c2bb
 
-export default { getAll }
+
+export default { getAll, handleToken: setToken }
